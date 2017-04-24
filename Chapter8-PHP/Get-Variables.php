7dc267e809fb8cc06c6e3648e6411e2a5f1e577c
Mingyuan Li <limin9yuan@gmail.com>
2017-04-23 22:10:08 -0400

    Done Get-Variables.php

--- Chapter8-PHP/Get-Variables.php
+++ Chapter8-PHP/Get-Variables.php
@@ -1,12 +1,52 @@
 <?php
+function isPrime($num) {
+  //1 is not prime. See: http://en.wikipedia.org/wiki/Prime_number#Primality_of_one
+  if($num == 1)
+      return false;
 
-  echo "Hi ".$_GET["name"]."!";
+  //2 is prime (the only even number that is prime)
+  if($num == 2)
+      return true;
 
+  /**
+   * if the number is divisible by two, then it's not prime and it's no longer
+   * needed to check other even numbers
+   */
+  if($num % 2 == 0) {
+      return false;
+  }
+
+  /**
+   * Checks the odd numbers. If any of them is a factor, then it returns false.
+   * The sqrt can be an aproximation, hence just for the sake of
+   * security, one rounds it to the next highest integer value.
+   */
+  $ceil = ceil(sqrt($num));
+  for($i = 3; $i <= $ceil; $i = $i + 2) {
+      if($num % $i == 0)
+          return false;
+  }
+
+  return true;
+}
+  if (isset($_GET["number"])) {
+    if (is_numeric($_GET["number"]) && $_GET["number"] > 0 && $_GET["number"] == round($_GET["number"], 0)) {
+      if (isPrime($_GET["number"])) {
+        echo $_GET["number"]." is prime number";
+      }
+      else {
+        echo $_GET["number"]." is not prime number";
+      }
+    }
+    else {
+      echo "Please enter a integer";
+    }
+  }
 
 ?>
 
-<p>What's your name?</p>
+<p>Please enter a number to check whether is prime number.</p>
 <form>
-  <input type="text" name="name">
+  <input type="text" name="number">
   <input type="submit" value="Go!">
 </form>
